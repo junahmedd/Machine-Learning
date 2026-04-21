69e93b8b6596b14a4766948e228c956eabd67537
Junaid Ahmed <mehjunaidd@gmail.com>
2026-04-21 20:11:55 +0530

    Handling Missing Values Encoding
    Fix issues with data preprocessing and imputer usage.

---
+++ hmve.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+dataset = pd.read_csv("<filename>")
+X = dataset.iloc[:,:-1].values
+Y = dataset.iloc[:,-1].values
+print(X,Y)
+
+from sklearn.impute import SimpleImputer 
+imputer = SimpleImputer(missing_values = np.nan, strategy = "mrean")
+imputer.fit(X[:, 1:3]).imputer_transform(X[:, 1:3])
+print(X)
+
+from sklearn.compose import ColumnTransformer
+from sklearn preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers = [('encoder', OneHotEncoder(), [0])], remainder = 'passthrough')
+X = np.array(ct.fit_transform(X))
+print(X)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(x,y, test_size = 0.2, random_state = 1)
+print(X_train, X_test, Y_train, Y_test)
+
+from sklearn preprocessing import StandardScaler
+sc = StandardScaler()
+X_train[:,3:] = sc.fit_transform(X_train[:,3:])
+X_test[:,3:] = sc.transform(X_test[:,3:])
+print(X_train)
+print(X_test)
